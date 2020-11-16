d01555149fc292437ab237bdc6a6dc6065acd4f2
Muneiahtellakula <Muneiahtellakula@gmail.com>
2020-11-16 10:27:36 +0530

    first day

---
+++ Class_Work.java
@@ -0,0 +1,143 @@
+A Good Morning To All 
+Welcome to Apssdc Android App Development Workshop
+I'm Muneiah Tellakula
+5 years -apssdc
+m.tech-jntua
+b.tech -cbit
+Mr.Sai Sankar
+Mr.Vara Prasad
+-------------Today Objective-----
+What is Android 
+History of the Android 
+Advatages 
+App buiding Stracture
+App Componets
+-------------
+Materails Link:
+https://developer.android.com/courses/fundamentals-training/overview-v2
+What is Android :
+An Android is Platform which consits of Mobile Oparating System based on the Linux kernal
+History of the Android:
+In 2003 Android Inc Company --idea -build OS on Digital Cameras
+1.Andy Rubin
+2.Rich Minar
+3.Crish white
+4.Nic Sear
+2005-->Company Went deep losses--
+diverted idea Digital cams->Mobile App
+2005-->Google Adapted Android Inc
+2007->from Google came first Android OS Mobile 
+first OS Mobile->HTC-Dream 
+Alpha
+Beta
+Cupcake
+Donut
+Eclar
+Froyo
+Ginger Bread
+Honey Comb
+Icecrem Sandwich
+Jelly Bean
+Kitkat
+LoliPop
+MarshMallow
+Nougat
+Oreo
+Pie
+Q-Android 10
+R-Android 11
+
+Heart-most import 
+Android Studio IDE-Intigrated Development Environment
+--------------------
+ Android Platform Architecture
+ 5 Layers 
+ 1.System Apps and User Apps
+ 	ex:System Apps are Inbuilt apps:messages,cam,cal,..etc
+ 	ex:User apps are dowload from playstore 
+ 		whtsup,fb,pubg..etc
+2.API Framework -java :it will provide the interface to user and system 
+3.Runtime or native lib..
+	while app runtime handle the some permisstions 
+		ex:internet permisstions ,microphone
+4.HAL:Hardware Abstration Layer
+	Camera,sensors--> provides interface b/w software Componets and hardware Componets
+	ex:Camera ->button
+5.Linux kernal:Share the all resourses
+	Resourses->power,memory,content..etc
+	---------
+App Development 
+What is an Android app?
+	One or more interactive screens
+
+HTML-hypertext markup language
+	Predefined Tags
+		<html> 
+				<title>
+
+				</title>
+				<Head>
+				</Head>
+				<Body>
+
+				</Body>
+		</html>
+XML-Extensible markup launge:screens Design purpose
+	user deined tags
+	
+	extrnal closing 			internal closing 
+	<laptop> 					<laptop/>
+	</laptop>
+
+	tv,watch,mobile phone,tab, and car
+----------------------------------
+App building blocks :4types
+Resourses:Images,String,etc
+Components:4 Components:
+			1.Acivity:An acivity is single Screen which consits of User interation
+				ex:one app screen 
+				in android terms:
+				Activity means java file (MainActivity.Java)
+				xml -Design
+
+				signle Activity coming from two files
+					first->java file(MainActivity.java)
+					seond->xml file(acivity_main.xml)
+			2.Service:A service is background long running process with out user interation 
+				ex:Music,Notification,Alarm ..etc
+			3.BroadCast Recivers:
+				Android Framework will provide some system wide assouncements 
+				ex:battry low,plug in the usb cable in the top displays the powerconneted symbol ..etc
+
+
+			ex:Radio Station -->Radio channels 
+				name:93.5,92.8..etc
+				when user can recive?
+				headset plugin then only we will recive the signals
+				through the anteena from headset
+
+			4.Content Provider:
+			it will provide the content between the two apps 
+			content:contacts,gallery file,audio files,video files,doc ..etc
+
+			Sai-9876543321
+
+	whtsup          hike                   
+	sai-9876543321  sai_myfrnd-9876543321
+
+messages                       messenges
+sai_mybestFrnd -9876543321  sai_janjigiri-9876543321
+ 
+
+Manifest:it will provde the information app componets and it will handle the runtime permissions
+Build configuration:It will have the enire app information
+	app versions
+	which sdk..
+Good Morning to all 
+
+Please go through this below link for all class work content 
+
+https://github.com/AP-Skill-Development-Corporation/AAD-STP-BATCH-27
+
+ Thank you ..
+ Muneiah Tellakula
