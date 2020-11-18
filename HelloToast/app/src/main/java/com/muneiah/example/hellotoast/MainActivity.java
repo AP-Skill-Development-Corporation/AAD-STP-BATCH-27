653a5d8ea19a4cb7201efecfbc8e8b8fe781b9aa
Muneiahtellakula <Muneiahtellakula@gmail.com>
2020-11-18 09:58:39 +0530

    hello toast app

---
+++ HelloToast/app/src/main/java/com/muneiah/example/hellotoast/MainActivity.java
@@ -0,0 +1,27 @@
+package com.muneiah.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+TextView label_text;
+int c=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        label_text=findViewById(R.id.tv);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, "Welcome android world", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showCount(View view) {
+        
+    }
+}
