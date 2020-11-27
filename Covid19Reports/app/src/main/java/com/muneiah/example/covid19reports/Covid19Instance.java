6573eb59aaee8c967adc0f203ad571e5f444332f
Muneiahtellakula <Muneiahtellakula@gmail.com>
2020-11-27 09:19:47 +0530

    Retrofit final commit

--- Covid19Reports/app/src/main/java/com/muneiah/example/covid19reports/Covid19Instance.java
+++ Covid19Reports/app/src/main/java/com/muneiah/example/covid19reports/Covid19Instance.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class Covid19Instance {
-    public static final String BASE_URL="https://api.covid19api.com/dayone/country/IN";
+    public static final String BASE_URL="https://api.covid19api.com";
     static Retrofit retrofit;
     public static Retrofit getInstance(){
         if (retrofit==null){
