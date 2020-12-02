02ecc92c953bdae7d36e32c9451948259d902de4
Muneiahtellakula <Muneiahtellakula@gmail.com>
2020-12-02 10:01:53 +0530

    Workmager with notification

--- OurNotification/app/src/main/java/com/muneiah/example/ournotification/MainActivity.java
+++ OurNotification/app/src/main/java/com/muneiah/example/ournotification/MainActivity.java
@@ -2,6 +2,9 @@ package com.muneiah.example.ournotification;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 
 import android.app.Notification;
 import android.app.NotificationChannel;
@@ -13,38 +16,46 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
-NotificationManager nm;
+/*NotificationManager nm;
 NotificationCompat.Builder builder;
 public static final int NOTIFICATION_ID=566654564;
 public static final String CHANNEL_ID="mychannel";
 PendingIntent pi;
-Intent i;
+Intent i;*/
+OneTimeWorkRequest onetime;
+PeriodicWorkRequest secondtime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        i=new Intent(this,MainActivity.class);
+        onetime=new OneTimeWorkRequest.Builder(FirstWork.class).build();
+        secondtime=new PeriodicWorkRequest.Builder(SecondWork.class,15, TimeUnit.MINUTES).build();
+       /* i=new Intent(this,MainActivity.class);
         pi=PendingIntent.getActivity(this,NOTIFICATION_ID,i,PendingIntent.FLAG_UPDATE_CURRENT);
         nm= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        createChannel();
+        createChannel();*/
     }
-    private void createChannel() {
+   /* private void createChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             NotificationChannel nc=new NotificationChannel(CHANNEL_ID,"apssdc",NotificationManager.IMPORTANCE_HIGH);
             nc.setLightColor(Color.BLUE);
             nc.enableVibration(true);
             nm.createNotificationChannel(nc);
         }
-    }
+    }*/
     public void showNotification(View view) {
-        builder=new NotificationCompat.Builder(this,CHANNEL_ID);
+        WorkManager.getInstance(this).enqueue(onetime);
+        WorkManager.getInstance(this).enqueue(secondtime);
+       /* builder=new NotificationCompat.Builder(this,CHANNEL_ID);
         builder.setContentText("This is our own Notification..!");
         builder.setContentTitle("My Notification");
         builder.setSmallIcon(R.drawable.ic_baseline_notifications_24);
         builder.setDefaults(Notification.DEFAULT_ALL);
         builder.setContentIntent(pi);
         builder.addAction(R.drawable.ic_baseline_reply_24,"Reply",pi);
-        nm.notify(NOTIFICATION_ID,builder.build());
+        nm.notify(NOTIFICATION_ID,builder.build());*/
     }
 }
